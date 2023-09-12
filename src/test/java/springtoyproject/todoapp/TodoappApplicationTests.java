68aafd52014e4c51a453263cea85f5c454beaef5
BH kang <rkdqudgjs1127@gmail.com>
2023-09-12 20:24:30 +0900

    Initial commit

---
+++ src/test/java/springtoyproject/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package springtoyproject.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
